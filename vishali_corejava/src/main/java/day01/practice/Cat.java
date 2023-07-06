99d0672762d3f78ace979fbc8c0d6a02e3df949c
vishali-30 <116253142+vishali-30@users.noreply.github.com>
2023-07-06 16:38:06 +0530

    Day01 commit

---
+++ vishali_corejava/src/main/java/day01/practice/Cat.java
@@ -0,0 +1,69 @@
+package day01.practice;
+
+public class Cat {
+	
+	  private int age;
+	  private String color;
+	  private String Speaks;
+
+
+    public int getAge() {
+		return age;
+	}
+
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+
+	public String getColor() {
+		return color;
+	}
+
+
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+
+
+	public String getSpeaks() {
+		return Speaks;
+	}
+
+
+
+	public void setSpeaks(String speaks) {
+		Speaks = speaks;
+	}
+
+
+
+	
+  
+  public static void main(String[] args) {
+      Cat cat1 = new Cat();
+      cat1.setColor("White");
+      cat1.setAge(4);
+      cat1.setSpeaks("Meow!");
+      Cat cat2 = new Cat();
+      cat2.setColor("Black");
+      cat2.setAge(3);
+      cat2.setSpeaks("Meow!");
+
+     
+      System.out.println("Cat 1:");
+      System.out.println("Age: " + cat1.getAge());
+      System.out.println("Color: " + cat1.getColor());
+      System.out.println("Color: " + cat1.getSpeaks());
+
+      System.out.println("Cat 2:");
+      System.out.println("Age: " + cat2.getAge());
+      System.out.println("Color: " + cat2.getColor());
+      System.out.println("Color: " + cat2.getSpeaks());
+  }
+}
