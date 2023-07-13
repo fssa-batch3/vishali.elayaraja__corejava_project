b5bf9593ca7d61490fe03f09f54e2f3f1fbba7b2
vishali-30 <116253142+vishali-30@users.noreply.github.com>
2023-07-13 10:17:48 +0530

    day06 commit

---
+++ vishali_corejava/src/main/java/day06/solved/ArrayListDemo.java
@@ -0,0 +1,23 @@
+package day06.solved;
+import java.util.ArrayList;
+	 
+	public class ArrayListDemo {
+	 
+	    //without using generics
+	    public static void main(String[] args) {
+	     
+	        // Store the city names in the List
+	        ArrayList<String> cityList  = new ArrayList <String>();
+	        cityList.add("Chennai");
+	        cityList.add("Bangalore");
+	        cityList.add("Mumbai");
+	         
+	        // Display the city names
+	        for (Object object : cityList) {
+	            String cityName = (String) object; // TypeCast object to String
+	            System.out.println(cityName);           
+	        }
+	 
+	    }
+	 
+	}
